763bff44b3ad618772a89833788d21c848253d71
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-04 12:38:08 -0700

    not functional yet, working with my hooks

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,8 +2,17 @@ import { useState } from 'react';
 
 const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-        return
+        const item = window.localStorage.getItem(key);
+        if (item) {
+            return JSON.parse(item);
+        } else {
+            return initialValue;
+        }
     });
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+    return [storedValue, setValue]
 };
-// useState(initialValue) ------> [value, setValue]
 export default useLocalStorage;
